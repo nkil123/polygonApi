71db3c89c9db0fa71f79233779fe719327e0720f
Nilesh <nkil123>
2023-07-27 23:30:04 +0530

    added api

--- server/app.js
+++ server/app.js
@@ -1,35 +1,61 @@
 // DO NOT MODIFY ANYTHING HERE, THE PLACE WHERE YOU NEED TO WRITE CODE IS MARKED CLEARLY BELOW
 
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require("body-parser");
-const axios = require("axios");
+require ('dotenv').config ();
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const axios = require ('axios');
 
-const app = express();
+const app = express ();
 
-app.use(function (req, res, next) {
-    const allowedOrigins = ['http://localhost:3000'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
+app.use (function (req, res, next) {
+  const allowedOrigins = ['http://localhost:3000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes (origin)) {
+    res.setHeader ('Access-Control-Allow-Origin', origin);
+  }
+  res.header (
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header ('Access-Control-Allow-credentials', true);
+  res.header ('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
+  next ();
 });
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
+app.use (
+  bodyParser.urlencoded ({
+    extended: true,
+  })
+);
+app.use (bodyParser.json ());
+const baseURL = 'https://api.polygon.io/v1';
+const api = axios.create ({
+  baseURL,
+});
+app.enable ('trust proxy');
 
-app.enable('trust proxy');
+app.get ('/api/fetchStockData', (req, res) => {
+  // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
+  const date = '2023-01-09';
+  const stockKey = 'AAPL';
+  const apiKey = 'wWkIKp1dwEvxcKx_UphNn3VMfTs1bhNC';
+  api
+    .get (`/open-close/${stockKey}/${date}/?adjusted=true&apiKey=${apiKey}`)
+    .then (response => {
+      // Handle the response data here
+      res.status (200).json (response.data);
+    })
+    .catch (error => {
+      // Handle errors here
+      console.error ('Error:', error.message);
+      res.status (500).send (error.message);
+    });
+});
 
-app.post('/api/fetchStockData', (req, res) => {
-    // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
-    res.sendStatus(200);
+app.get ('/', () => {
+  console.log ('hlell');
+  return;
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen (port, () => console.log (`Listening on port ${port}`));
